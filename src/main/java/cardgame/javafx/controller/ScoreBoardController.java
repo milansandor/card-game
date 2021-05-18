3ca9509cb7811fb7c2e857c57ff97f103153c810
milansandor <milol66@icloud.com>
2021-05-18 12:27:58 +0200

    ScoreBoard prints out the results correctly

--- src/main/java/cardgame/javafx/controller/ScoreBoardController.java
+++ src/main/java/cardgame/javafx/controller/ScoreBoardController.java
@@ -6,13 +6,39 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ScoreBoardController {
+
+    private static List<Integer> playerOneScores = new ArrayList<>();
+    private static List<Integer> playerTwoScores = new ArrayList<>();
+
+    private static void parseGameObject(JSONObject game)
+    {
+        //Get gamee object within list
+        JSONObject gameObject = (JSONObject) game.get("game");
+
+        //Get game playerOneResult
+        Long playerOneResult = (Long) gameObject.get("Player One");
+        // add to playerOneScores List
+        playerOneScores.add(playerOneResult.intValue());
+
+        //Get game playerTwoResult
+        Long playerTwoResult = (Long) gameObject.get("Player Two");
+        // add to playerTwoScores List
+        playerTwoScores.add(playerTwoResult.intValue());
+    }
 
     @FXML
     private TableView<Integer> scoreTable;
@@ -26,8 +52,25 @@ public class ScoreBoardController {
 
     @FXML
     private void initialize() {
-        List<Integer> playerOneScores = Arrays.asList(1,2,3);
-        List<Integer> playerTwoScores = Arrays.asList(2,2,3);
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader("file.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+
+            JSONArray gameList = (JSONArray) obj;
+            //System.out.println(gameList);
+
+            //Iterate over employee array
+            gameList.forEach( gam -> parseGameObject( (JSONObject) gam ) );
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         for (int i = 0; i < playerOneScores.size() && i < playerTwoScores.size(); i++) {
             scoreTable.getItems().add(i);
